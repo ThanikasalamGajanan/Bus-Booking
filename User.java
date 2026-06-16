71b00ca272b9ddaab465c410415088fa564a067a
Thanikasalam Gajanan <thanikasalamgajanan@gmail.com>
2026-06-16 13:51:08 +0530

    doc: Add JavaDoc documentation to Bus Class

---
+++ User.java
@@ -0,0 +1,13 @@
+package busReservationSystem;
+
+public abstract class User {
+    protected String name;
+    protected String phone;
+
+    public User(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public abstract void displayInfo();
+}
