f625b313c9f1e964652329d97dde5a6bc0d05a08
ahamedakram028 <ahamedakram028@gmail.com>
2026-06-17 11:03:19 +0530

    doc: Add JavaDoc documentation to User Class

---
+++ src/busReservationSystem/AvailabilityChecker.java
@@ -0,0 +1,19 @@
+package busReservationSystem;
+
+import java.util.ArrayList;
+
+public class AvailabilityChecker {
+
+    public static Bus findBus(ArrayList<Bus> buses, String busNo) {
+        for (Bus b : buses) {
+            if (b.getVehicleNo().equals(busNo)) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public static boolean checkSeats(Bus bus, int seatsRequested) {
+        return bus != null && bus.getSeatCount() >= seatsRequested;
+    }
+}
