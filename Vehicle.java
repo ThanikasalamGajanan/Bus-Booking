2524d97c6c407c0fa41bb440328bf1139ec24fdd
Vakeesh809 <vakeesh809@gmail.com>
2026-06-18 00:02:04 +0530

    --add meaningful comments to Vehicle class

--- Vehicle.java
+++ Vehicle.java
@@ -1,53 +1,30 @@
 package busReservationSystem;
-/**
- * Abstract base class representing a vehicle in the reservation system.
- * Common properties such as vehicle number and seat count are defined here.
- * Specific vehicle types must implement the display() method.
- */
 
+// Base class for all vehicles
 public abstract class Vehicle {
 
-    // Unique identifier of the vehicle (e.g., bus number)
+    // Vehicle number
     protected String vehicleNo;
 
-    // Total number of seats available in the vehicle
+    // Number of seats
     protected int seatCount;
 
-    /**
-     * Creates a Vehicle object with the given vehicle number and seat capacity.
-     *
-     * @param vehicleNo Unique vehicle number
-     * @param seatCount Total seating capacity of the vehicle
-     */
-
+    // Constructor
     protected Vehicle(String vehicleNo, int seatCount) {
         this.vehicleNo = vehicleNo;
         this.seatCount = seatCount;
     }
 
-    /**
-     * Returns the vehicle number.
-     *
-     * @return vehicle numbers
-     */
+    // Get vehicle number
     public String getVehicleNo() {
         return vehicleNo;
     }
 
-    /**
-     * Returns the total seat capacity of the vehicle.
-     *
-     * @return number of seat
-     */
-
+    // Get seat count
     public int getSeatCount() {
         return seatCount;
     }
 
-    /**
-     * Displays vehicle details.
-     * Must be implemented by all subclasses.
-     */
-
+    // Display vehicle details
     public abstract void display();
 }
